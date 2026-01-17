ad3e60451579388241de000e62cab15dbd01aa57
vecchia001 <motivaction.contacts@gmail.com>
2026-01-17 18:31:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/venezia-chauffeur-site-main/',
   server: {
     host: "::",
     port: 8080,
